a24f534afb9f09af7159c940df60f29f3a49bc0a
subhramall <subhrajyoti.mall@praxis.ac.in>
2022-05-09 01:21:07 +0530

    Removed subfolder

--- main.py
+++ main.py
@@ -22,12 +22,12 @@ from flask import Flask, render_template, request, Markup
 
 
 # Loading the crop Recommendation Model
-crop_recommendation_model_path = 'models/crop_final.pkl'
+crop_recommendation_model_path = 'crop_final.pkl'
 crop_recommendation_model = pickle.load(
     open(crop_recommendation_model_path, 'rb'))
 
 # Loading the Crop Yield Recommendation model pickle file
-pickled_model_file = open('models/yield_prediction_final.pkl','rb')
+pickled_model_file = open('yield_prediction_final.pkl','rb')
 classifier = pickle.load(pickled_model_file)
 
 
